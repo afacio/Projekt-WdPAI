56dfd1997a7741e79096e55a715f077329baad44
afacio <afacio@users.noreply.github.com>
2021-01-23 21:54:05 +0100

    Create start.php

---
+++ public/views/start.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+    <title>START PAGE</title>
+    <link rel="stylesheet" type="text/css" href="public/css/start-style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+    <div class="container">
+        <div class="logo">
+            <img src="public/img/duze-logo-z-napisem.svg" class="respo">
+        </div>
+        <div class="powitanie">
+            Witaj, aby przejść dalej zaloguj się<br/> bądź utwórz konto
+        </div>
+        <form>
+            <button ><a href="login">LOGIN</a></button>
+            <a><button href="register">CREATE ACCOUNT</button></a>
+        </form>
+    </div>
+</body>
