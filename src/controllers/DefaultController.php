d82f35aa380a4a80cb375bec7a5fdf0bd8d15434
afacio <afacio@users.noreply.github.com>
2021-01-24 12:08:38 +0100

    Update DefaultController.php

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -4,8 +4,23 @@ require_once 'AppController.php';
 
 class DefaultController extends AppController {
 
-    public function index()
+    public function login()
     {
         $this->render('login');
+    }
+
+    public function register()
+    {
+        $this->render('register');
+    }
+
+    public function home()
+    {
+        $this->render('home');
+    }
+
+    public function start()
+    {
+        $this->render('start');
     }
 }
