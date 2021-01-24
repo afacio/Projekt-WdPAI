a56bbfbd793eaf31a48723076fc93f5de462ce1e
afacio <afacio@users.noreply.github.com>
2021-01-24 12:06:59 +0100

    Create galeria.php

---
+++ public/views/galeria.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<head>
+    <title>ODKRYJ</title>
+    <link rel="stylesheet" type="text/css" href="public/css/galeria-style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+    <div class="container">
+        <div class="logo">
+            <img src="public/img/male-logo-z-napisem.svg" class="male-logo">
+        </div>
+        <div class=galeria-container>
+            <div class="kategorie">
+                <h1 style=" margin: 2vh;">KATEGORIE</h1>
+                <form>
+                    <button class="ketegoria">Kategoria 1</button>
+                    <button class="ketegoria">Kategoria 2</button>
+                    <button class="ketegoria">Kategoria 3</button>
+                    <button class="ketegoria">Kategoria 4</button>
+                    <button class="ketegoria">Kategoria 5</button>
+
+                </form>
+            </div>
+            <div class="uklad">
+                <h1 style="margin: 2vh;">ODKRYJ</h1>
+                <div class="container-obrazy-poziom">
+                    <div class="kafelek">
+
+                    </div>
+                    <div class="kafelek" >
+
+                    </div>
+                    <div class="kafelek">
+
+                    </div>
+                </div>
+                <div class="container-obrazy-poziom">
+                    <div class="kafelek">
+
+                    </div>
+                    <div class="kafelek">
+
+                    </div>
+                    <div class="kafelek" >
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
