6ed0a7d0946f4e69bb37f9c2b8ab48f9cfa5d7dc
afacio <afacio@users.noreply.github.com>
2021-01-26 17:52:00 +0100

    Update ProjectController.php

--- src/controllers/ProjectController.php
+++ src/controllers/ProjectController.php
@@ -2,6 +2,7 @@
 
 require_once 'AppController.php';
 require_once __DIR__ .'/../models/Project.php';
+require_once __DIR__.'/../repository/ProjectRepository.php';
 
 class ProjectController extends AppController {
 
@@ -10,16 +11,25 @@ class ProjectController extends AppController {
     const UPLOAD_DIRECTORY = '/../public/uploads/';
 
     private $message = [];
+    private $projectRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->projectRepository = new ProjectRepository();
+    }
 
     public function addProject()
     {
         if ($this->isPost() && is_uploaded_file($_FILES['file']['tmp_name']) && $this->validate($_FILES['file'])) {
             move_uploaded_file(
                 $_FILES['file']['tmp_name'],
-                dirname(__DIR__).self::UPLOAD_DIRECTORY.$_FILES['file']['name']);
+                dirname(__DIR__).self::UPLOAD_DIRECTORY.$_FILES['file']['name']
+            );
 
             // TODO create new project object and save it in database
             $project = new Project($_POST['title'], $_FILES['file']['name']);
+            $this->projectRepository->addProject($project);
 
             return $this->render('home', ['messages' => $this->message]);
         }
