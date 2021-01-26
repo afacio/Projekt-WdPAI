3bb54e8bc3e7dbc3b8df2b1cb12df506aa61235f
afacio <afacio@users.noreply.github.com>
2021-01-26 11:50:03 +0100

    Update SecurityController.php

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -6,24 +6,18 @@ require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController {
 
-    private $userRepository;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->userRepository = new UserRepository();
-    }
-
     public function login()
     {
+        $userRepository = new UserRepository();
+
         if (!$this->isPost()) {
             return $this->render('login');
         }
 
         $email = $_POST['email'];
-        $password = md5($_POST['password']);
+        $password = $_POST['password'];
 
-        $user = $this->userRepository->getUser($email);
+        $user = $userRepository->getUser($email);
 
         if (!$user) {
             return $this->render('login', ['messages' => ['User not found!']]);
@@ -38,32 +32,6 @@ class SecurityController extends AppController {
         }
 
         $url = "http://$_SERVER[HTTP_HOST]";
-        header("Location: {$url}/home");
-    }
-
-    public function register()
-    {
-        if (!$this->isPost()) {
-            return $this->render('register');
-        }
-
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $confirmedPassword = $_POST['confirmedPassword'];
-        $name = $_POST['name'];
-        $surname = $_POST['surname'];
-        $phone = $_POST['phone'];
-
-        if ($password !== $confirmedPassword) {
-            return $this->render('register', ['messages' => ['Please provide proper password']]);
-        }
-
-        //TODO try to use better hash function
-        $user = new User($email, md5($password), $name, $surname);
-        $user->setPhone($phone);
-
-        $this->userRepository->addUser($user);
-
-        return $this->render('login', ['messages' => ['You\'ve been succesfully registrated!']]);
+        header("Location: {$url}/projects");
     }
 }
