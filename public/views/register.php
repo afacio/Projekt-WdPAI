2c99c18e415b9184d1651b4e85e55f938b834371
afacio <afacio@users.noreply.github.com>
2021-01-27 11:50:57 +0100

    Update register.php

--- public/views/register.php
+++ public/views/register.php
@@ -3,31 +3,34 @@
     <title>REGISTER PAGE</title>
     <link rel="stylesheet" type="text/css" href="public/css/register-style.css">
     <link rel="stylesheet" type="text/css" href="public/css/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="./public/js/script.js" defer></script>
 </head>
 <body>
     <div class="container">
         <div class="logo">
             <img src="public/img/duze-logo-z-napisem.svg" class="respo">
         </div>
-        
+
         <div class="login-container">
-						<form class="register" action="register" method="POST">
-							<?php
-								if(isset($messages)){
-									foreach($messages as $message) {
-										echo $message;
-									}
-								}
-							?>
-                            <input name="name" type="text" placeholder="name">
-                            <input name="surname" type="text" placeholder="surname">
-							<input name="email" type="text" placeholder="email@email.com">
-							<input name="password" type="password" placeholder="password">
-							<input name="confirmedPassword" type="password" placeholder="confirm password">
-							<button type="submit">REGISTER</button>
-						</form>
-					</div>
+            <form class="register" action="register" method="POST">
+                <div class="messages">
+                    <?php
+                    if(isset($messages)){
+                        foreach($messages as $message) {
+                            echo $message;
+                        }
+                    }
+                    ?>
+                </div>
+                <input name="name" type="text" placeholder="name">
+                <input name="surname" type="text" placeholder="surname">
+                <input name="email" type="text" placeholder="email@email.com">
+                <input name="password" type="password" placeholder="password">
+                <input name="confirmedPassword" type="password" placeholder="confirm password">
+
+                <button type="submit">REGISTER</button>
+            </form>
+        </div>
        
     </div>
 </body>
