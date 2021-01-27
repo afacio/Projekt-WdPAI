c9597e970903eafc88a1e480f31d27415c77d55c
afacio <afacio@users.noreply.github.com>
2021-01-27 18:13:48 +0100

    Update SecurityController.php

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -58,7 +58,6 @@ class SecurityController extends AppController {
             return $this->render('register', ['messages' => ['Please provide proper password']]);
         }
 
-        //TODO try to use better hash function
         $user = new User($email, md5($password), $name, $surname);
         $user->setPhone($phone);
 
