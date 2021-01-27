6db5448dc7fa1d3ccadf1eec9d3517a39b1ec487
afacio <afacio@users.noreply.github.com>
2021-01-27 11:51:07 +0100

    Update UserRepository.php

--- src/repository/UserRepository.php
+++ src/repository/UserRepository.php
@@ -27,4 +27,40 @@ class UserRepository extends Repository
             $user['surname']
         );
     }
+    public function addUser(User $user)
+    {
+        $stmt = $this->database->connect()->prepare('
+            INSERT INTO users_details (name, surname)
+            VALUES (?, ?)
+        ');
+
+        $stmt->execute([
+            $user->getName(),
+            $user->getSurname(),
+        ]);
+
+        $stmt = $this->database->connect()->prepare('
+            INSERT INTO users (email, password, id_user_details)
+            VALUES (?, ?, ?)
+        ');
+
+        $stmt->execute([
+            $user->getEmail(),
+            $user->getPassword(),
+            $this->getUserDetailsId($user)
+        ]);
+    }
+
+    public function getUserDetailsId(User $user): int
+    {
+        $stmt = $this->database->connect()->prepare('
+            SELECT * FROM public.users_details WHERE name = :name AND surname = :surname');
+        $stmt->bindParam(':name', $user->getName(), PDO::PARAM_STR);
+        $stmt->bindParam(':surname', $user->getSurname(), PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $data['id'];
+    }
 }
