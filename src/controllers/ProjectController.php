d842eabc3b1e8d3f507366bc8fbb2a0ff2bcaa4a
afacio <afacio@users.noreply.github.com>
2021-01-27 17:39:30 +0100

    Update ProjectController.php

--- src/controllers/ProjectController.php
+++ src/controllers/ProjectController.php
@@ -1,12 +1,13 @@
 <?php
 
 require_once 'AppController.php';
-require_once __DIR__ .'/../models/Project.php';
-require_once __DIR__.'/../repository/ProjectRepository.php';
+require_once __DIR__ . '/../models/Project.php';
+require_once __DIR__ . '/../repository/ProjectRepository.php';
 
-class ProjectController extends AppController {
+class ProjectController extends AppController
+{
 
-    const MAX_FILE_SIZE = 1024*1024;
+    const MAX_FILE_SIZE = 1024 * 1024;
     const SUPPORTED_TYPES = ['image/png', 'image/jpeg'];
     const UPLOAD_DIRECTORY = '/../public/uploads/';
 
@@ -19,23 +20,56 @@ class ProjectController extends AppController {
         $this->projectRepository = new ProjectRepository();
     }
 
-    public function upload()
+    public function projects()
+    {
+        $projects = $this->projectRepository->getProjects();
+        $this->render('projects', ['projects' => $projects]);
+    }
+
+    public function addProject()
     {
         if ($this->isPost() && is_uploaded_file($_FILES['file']['tmp_name']) && $this->validate($_FILES['file'])) {
             move_uploaded_file(
                 $_FILES['file']['tmp_name'],
-                dirname(__DIR__).self::UPLOAD_DIRECTORY.$_FILES['file']['name']
+                dirname(__DIR__) . self::UPLOAD_DIRECTORY . $_FILES['file']['name']
             );
 
             // TODO create new project object and save it in database
             $project = new Project($_POST['title'], $_FILES['file']['name']);
             $this->projectRepository->addProject($project);
 
-            return $this->render('home', [
-                'projects' => $this->projectRepository->getProjects(),
-                'messages' => $this->message]);
+            return $this->render('galeria', [
+                'messages' => $this->message,
+                'projects' => $this->projectRepository->getProjects()
+            ]);
         }
-        return $this->render('upload', ['messages' => $this->message]);
+
+        return $this->render('add-project', ['messages' => $this->message]);
+    }
+
+    public function search()
+    {
+        $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+
+        if ($contentType === "application/json") {
+            $content = trim(file_get_contents("php://input"));
+            $decoded = json_decode($content, true);
+
+            header('Content-type: application/json');
+            http_response_code(200);
+
+            echo json_encode($this->projectRepository->getProjectByTitle($decoded['search']));
+        }
+    }
+
+    public function like(int $id) {
+        $this->projectRepository->like($id);
+        http_response_code(200);
+    }
+
+    public function dislike(int $id) {
+        $this->projectRepository->dislike($id);
+        http_response_code(200);
     }
 
     private function validate(array $file): bool
@@ -50,12 +84,5 @@ class ProjectController extends AppController {
             return false;
         }
         return true;
-    }
-
-    public function galeria()
-    {
-        $projects = $this->projectRepository->getProjects();
-        $this->render('galeria', ['projects' => $projects]);
-
     }
 }
