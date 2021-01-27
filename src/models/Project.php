9689ea8dabe7ff63e7fed852287da18f7c373a3a
afacio <afacio@users.noreply.github.com>
2021-01-27 17:39:37 +0100

    Update Project.php

--- src/models/Project.php
+++ src/models/Project.php
@@ -1,15 +1,20 @@
 <?php
 
-class Project {
+class Project
+{
     private $title;
     private $image;
     private $like;
     private $dislike;
+    private $id;
 
-    public function __construct($title, $image)
+    public function __construct($title, $image, $like = 0, $dislike = 0, $id = null)
     {
         $this->title = $title;
         $this->image = $image;
+        $this->like = $like;
+        $this->dislike = $dislike;
+        $this->id = $id;
     }
 
     public function getTitle()
@@ -30,5 +35,35 @@ class Project {
     public function setImage($image)
     {
         $this->image = $image;
+    }
+
+    public function getLike(): int
+    {
+        return $this->like;
+    }
+
+    public function setLike(int $like): void
+    {
+        $this->like = $like;
+    }
+
+    public function getDislike(): int
+    {
+        return $this->dislike;
+    }
+
+    public function setDislike(int $dislike): void
+    {
+        $this->dislike = $dislike;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id): void
+    {
+        $this->id = $id;
     }
 }
