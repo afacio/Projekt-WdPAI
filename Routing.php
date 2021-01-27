9c7256b82149010bf5ba54a5c4d7ee1b01b48361
afacio <afacio@users.noreply.github.com>
2021-01-27 17:39:23 +0100

    Update Routing.php

--- Routing.php
+++ Routing.php
@@ -4,20 +4,27 @@ require_once 'src/controllers/DefaultController.php';
 require_once 'src/controllers/SecurityController.php';
 require_once 'src/controllers/ProjectController.php';
 
-class Router {
+class Router
+{
 
     public static $routes;
 
-    public static function get($url, $view) {
+    public static function get($url, $view)
+    {
         self::$routes[$url] = $view;
     }
 
-    public static function post($url, $view) {
+    public static function post($url, $view)
+    {
         self::$routes[$url] = $view;
     }
 
-    public static function run ($url) {
-        $action = explode("/", $url)[0];
+    public static function run($url)
+    {
+
+        $urlParts = explode("/", $url);
+        $action = $urlParts[0];
+
         if (!array_key_exists($action, self::$routes)) {
             die("Wrong url!");
         }
@@ -26,6 +33,8 @@ class Router {
         $object = new $controller;
         $action = $action ?: 'index';
 
-        $object->$action();
+        $id = $urlParts[1] ?? '';
+
+        $object->$action($id);
     }
 }
