f9237c8e16f288cfedadab9e270ee95d40e5a565
afacio <afacio@users.noreply.github.com>
2021-01-27 10:05:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
 Router::get('', 'DefaultController');
-Router::get('home', 'DefaultController');
+Router::get('home', 'ProjectController');
 Router::get('start', 'DefaultController');
 //Router::get('galeria', 'DefaultController');
 //Router::get('odkryj', 'DefaultController');
