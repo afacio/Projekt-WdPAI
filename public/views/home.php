b6757da70211405766606e1b8d30f23cd1d8c6fe
afacio <afacio@users.noreply.github.com>
2021-01-28 19:21:13 +0100

    Update home.php

--- public/views/home.php
+++ public/views/home.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <head>
     <title>HOME PAGE</title>
-    <link rel="stylesheet" type="text/css" href="public/css/home-style.css">
-    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+
+    <link rel="stylesheet" type="text/css" href="public/css/home.css">
+
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <script type="text/javascript" src="./public/js/search.js" defer></script>
@@ -11,6 +13,7 @@
 </head>
 <body>
     <div class="container">
+
         <div class="pasek">
             <a class="kostka" href="addProject">
                 <img src="public/img/plus.svg">
@@ -18,45 +21,47 @@
             <a class="kostka" href="/logout">
                 <img src="public/img/logout.svg">
             </a>
-            
-            <img src="public/img/male-logo-z-napisem.svg" class="male-logo">
-            
+            <a href="home">
+                <img src="public/img/male-logo-z-napisem.svg" class="male-logo">
+            </a>
+
             <a class="kostka" href="https://www.instagram.com/" target="_blank">
                 <img src="public/img/insta.svg">
             </a>
             <a class="kostka" href="https://www.facebook.com/" target="_blank">
                 <img src="public/img/fb.svg">
             </a>
-            
-        </div>
-        <div class="search-barr">
-            <form class="search-bar">
-                <input placeholder="search image">
-            </form>
-        </div>
-        <div class=galeria>
-            <section class="kafelek">
-                <?php foreach ($projects as $project): ?>
-                    <div id="<?= $project->getId(); ?>">
-                        <img class="mem" src="public/uploads/<?= $project->getImage(); ?>">
 
-                    </div>
-                <?php endforeach; ?>
-            </section>
         </div>
+        <div class="formy" >
+            <form class="form" action="projects" method="POST">
+                <button class="przycisk" style="
+            background: #faf3eb ;
+            border: 1px solid #49494967;
+            border-radius: 17px;
+            opacity: 0.5;
+            height: 6vh;
+            width: auto;
+            min-width: 30vw;
+            margin: 2vw;">
+                    <a href="projects">Odkryj</a>
+                </button>
+            </form>
+            <form class="form" action="projectsLogged" method="POST">
+                <button class="przycisk" style="
+            background: #faf3eb ;
+            border: 1px solid #49494967;
+            border-radius: 17px;
+            opacity: 0.5;
+            height: 6vh;
+            width: auto;
+            min-width: 30vw;
+            margin: 2vw;">
+                    <a href="projectsLogged">Galeria</a>
+                </button>
+            </form>
+
         </div>
+
     </div>
 </body>
-<template id="project-template">
-    <div id="">
-        <img src="">
-        <div>
-            <h2>title</h2>
-            <p>description</p>
-            <div class="social-section">
-                <i class="fas fa-heart"> 0</i>
-                <i class="fas fa-minus-square"> 0</i>
-            </div>
-        </div>
-    </div>
-</template>
