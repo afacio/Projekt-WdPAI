553f72051307c8875d5576aa28bfb7ba41e863ef
afacio <afacio@users.noreply.github.com>
2021-01-28 19:33:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,14 +11,13 @@ Router::get('', 'DefaultController');
 Router::get('home', 'DefaultController');
 Router::get('start', 'DefaultController');
 Router::get('galeria', 'ProjectController');
-//Router::get('odkryj', 'DefaultController');
+
 Router::post('login', 'SecurityController');
 Router::post('logout', 'SecurityController');
 Router::post('register', 'SecurityController');
 Router::post('addProject', 'ProjectController');
 Router::post('search', 'ProjectController');
-Router::get('like', 'ProjectController');
-Router::get('dislike', 'ProjectController');
+
 Router::get('projects', 'ProjectController');
 Router::get('projectsLogged', 'ProjectController');
 
