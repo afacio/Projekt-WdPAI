450f971a23842ca31236256dfa6d7714813c4853
afacio <afacio@users.noreply.github.com>
2021-01-28 19:21:11 +0100

    Update add-project.php

--- public/views/add-project.php
+++ public/views/add-project.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <head>
     <title>HOME PAGE</title>
-    <link rel="stylesheet" type="text/css" href="public/css/home-style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/project.css">
     <link rel="stylesheet" type="text/css" href="public/css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -12,11 +12,12 @@
 
             <a href="home">   <img src="public/img/male-logo-z-napisem.svg" class="male-logo"></a>
 
-            
         </div>
-        <div class="tresc" style="clear: both; width: 100%; height: auto;">
-            <div class=galeria>
-                <h1>UPLOAD</h1>
+        <div class=galeria>
+            <div class="tekst">
+                Dodaj swoje ulubione memy do kolekcji</br>i podziel sie nimi z innymi
+            </div>
+            <div class="upload">
                 <form action="addProject" method="POST" ENCTYPE="multipart/form-data">
                     <div class="messages">
                         <?php
@@ -27,13 +28,15 @@
                         }
                         ?>
                     </div>
-                    <input name="title" type="text" placeholder="title">
+                    <input class="title" name="title" type="text" placeholder="title">
 
 
-                    <input type="file" name="file"/><br/>
+                    <input class="file" type="file" name="file"/><br/>
                     <button type="submit">send</button>
                 </form>
             </div>
+
         </div>
+
     </div>
 </body>
